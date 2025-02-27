54da4d1b4c929ce56dbec7d2706edf18e53f62e5
qpostman <qdpostma@syr.edu>
2025-02-27 12:00:07 -0500

    lab07

---
+++ lab07/example.js
@@ -0,0 +1,11 @@
+console.log("Hello from the External File");
+
+let gpa = 3.4;
+    let deansList;
+    if (gpa >= 3.5) {
+        deansList = "Dean's List";
+    }
+    else if (gpa < 3.5) {
+        deansList = "No Dean's List";
+    }
+    console.log (deansList);
